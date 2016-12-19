afe50b78acc03712efac07019df038d6794c3ff7
zivlA <raphviz@gmail.com>
2016-12-18 23:29:36 -0500

    post to db

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ app.set("view engine", "ejs");
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
@@ -30,7 +31,7 @@ app.get('/campgrounds', function(req, res) {
         if (err) {
             console.log(err);
         } else {
-            res.render('campgrounds', {campgrounds: allCampgrounds});
+            res.render('index', {campgrounds: allCampgrounds});
         }
     })
 });
@@ -38,7 +39,8 @@ app.get('/campgrounds', function(req, res) {
 app.post('/campgrounds', function(req, res) {
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc};
     // Create new campground and save to db
     Campground.create(newCampground, function(err, newlyCreated) {
         if(err) {
@@ -51,6 +53,17 @@ app.post('/campgrounds', function(req, res) {
 
 app.get('/campgrounds/new', function(req, res) {
     res.render("new.ejs");
+});
+
+// SHOW - shows more info
+app.get('/campgrounds/:id', function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("show", {campground: foundCampground});
+        }
+    })
 });
 
 app.listen(3000, function() {
